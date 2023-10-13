18002b3163a060f8d02bbd134ca14e3c27b7c166
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-13 12:24:31 +0200

    setup for project

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// I config/config.js
+require('dotenv').config();
+
+module.exports = {
+  database: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
+  // andra globala konfigurationer kan gå här i framtiden
+};
